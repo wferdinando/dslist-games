f54185bb1fbc406da1c8d8fdc2ca7a293e220017
Willyan Ferdinando <willyantferdinando@gmail.com>
2023-07-10 21:32:04 -0300

    Project Created

---
+++ src/test/java/io/wferdinando/dslist/DslistApplicationTests.java
@@ -0,0 +1,13 @@
+package io.wferdinando.dslist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DslistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
