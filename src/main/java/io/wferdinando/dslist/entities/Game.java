1614200cc6fa704247843fdeffbe4910985c8e33
Willyan Ferdinando <willyantferdinando@gmail.com>
2023-07-10 22:15:13 -0300

    Game seed

--- src/main/java/io/wferdinando/dslist/entities/Game.java
+++ src/main/java/io/wferdinando/dslist/entities/Game.java
@@ -3,6 +3,7 @@ package io.wferdinando.dslist.entities;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
@@ -11,7 +12,7 @@ import jakarta.persistence.Table;
 public class Game {
     
     @Id
-    @GeneratedValue
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String title;
@@ -27,8 +28,10 @@ public class Game {
 
     private String imgUrl;
 
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
 
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Game(){}
